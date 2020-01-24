c7b239868d17dc1bf68fdd6553c0267f16185259
Peter Cestrone <Christina.Zittinger@Cabarrus.K12.nc.us>
2020-01-24 12:17:23 -0500

    Interface loops until good option picked

--- cholesterol_analysis.py
+++ cholesterol_analysis.py
@@ -1,11 +1,13 @@
 # interface
-def interfact():
-    print("Cholesterol Calculator")
-    print("Options: ")
-    print(" 9  - Quit")
-    choice = input("Enter your options: ")
-    if choice == "9":
-        return
+def interface():
+    choice = 0
+    while True:
+        print("Cholesterol Calculator")
+        print("Options: ")
+        print(" 9  - Quit")
+        choice = input("Enter your options: ")
+        if choice == "9":
+            return
         
 if __name__ == "__main__":
     interface()
