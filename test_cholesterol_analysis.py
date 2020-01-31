da19d88387948427fd2673b201a23011babe4ece
Peter Cestrone <Christina.Zittinger@Cabarrus.K12.nc.us>
2020-01-31 12:38:09 -0500

    added HDL and LDL test

--- test_cholesterol_analysis.py
+++ test_cholesterol_analysis.py
@@ -1,6 +1,21 @@
 #Test
 def test_HDL_analysis():
 	from cholesterol_analysis import HDL_analysis
-	answer = DHL_analysis(80)
+	answer = HDL_analysis(80)
 	expected = "Normal"
 	assert answer == expected
+
+
+def test_HDL_analysis():
+	from cholesterol_analysis import HDL_analysis
+	answer = HDL_analysis(40)
+	expected = "Borderline low"
+	assert answer == expected
+   
+
+def test_LDL_analysis():
+	from cholesterol_analysis import LDL_analysis
+	answer = LDL_analysis(165)
+	expected = "High"
+	assert answer == expected
+
